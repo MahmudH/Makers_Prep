d0ed047e0760737e3b1e34896ee32499b7fd81a7
mahmudh <mahmudhssn246@gmail.com>
2015-08-13 18:39:13 +0100

    calc.rb add

---
+++ calc.rb
@@ -0,0 +1,27 @@
+puts 1 + 2
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+puts 5 * (12-8) + -15
+
+#365 days in a year and each day is 24 hours, therefore 365 days multiplied by hours which is 24
+puts 365 * 24
+
+#365 days in 1 year, decade is 10 years, therfore 365 multiplied by 10 is the amount of days in a decade
+#there are 60 minutes in 1 hour, and there are 24 hours in a day, therefore multiply amount of days with 24 and then times by 60 for minutes
+
+# mintutes in one year including leap year
+year_minutes = (365.25 * 24) * 60
+
+puts "Hours in a year are: #{year_minutes}"
+
+#decade is 10 years, year_minutes multiplied by 10 is minutes in a decade
+ decade_minutes = year_minutes * 10
+
+ puts "Minutes in a decade are: #{decade_minutes}"
+
+ #how many seconds old am I? 
+ #
+
+
